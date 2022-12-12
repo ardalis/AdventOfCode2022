306e17fea1d1f1f64b0601766a6bc4928e452d1f
Steve Smith <steve@kentsmiths.com>
2022-12-12 01:20:05 -0500

    Working on Day 12

---
+++ src/AdventOfCode2022/Day12/Map.cs
@@ -0,0 +1,23 @@
+﻿namespace AdventOfCode2022.Day012;
+
+public class Map
+{
+    public List<List<char>> Rows { get; } = new List<List<char>>();
+
+    public static Map Parse(string input)
+    {
+        var map = new Map();
+        var rows = input.Split(Environment.NewLine);
+        foreach (var row in rows)
+        {
+            map.Rows.Add(row.ToCharArray().ToList());
+        }
+
+        return new Map();
+    }
+
+    public char this[int col, int row]
+    {
+        get { return Rows![row][col]; }
+    }
+}
---
+++ src/AdventOfCode2022/Day12/Tests/MapParse.cs
@@ -0,0 +1,15 @@
+﻿using AdventOfCode2022.Day012;
+
+namespace AdventOfCode2022.Day12.Tests
+{
+    public class MapParse
+    {
+        [Fact]
+        public void ReturnsCorrectSampleMap()
+        {
+            var map = Map.Parse(Input.SampleData);
+
+            Assert.Equal('S', map[0,0]);
+        }
+    }
+}
